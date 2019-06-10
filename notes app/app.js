4a8a8c75746943eb7e8a37e616de291b79f85560
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-06-10 23:38:20 +0530

    use of appendFileSync

--- notes app/app.js
+++ notes app/app.js
@@ -1,3 +1,9 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt','This file was created by node.js')
+fs.writeFileSync('notes.txt','My name is DK\n')
+
+//Challenge: Append a message to notes.js
+
+//Use appendFileSync to append the file
+
+fs.appendFileSync('notes.txt', 'This is the appended txt')
