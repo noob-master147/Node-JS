42f12fd8e5aea6b6e515d0f80d7f3a8854b1c217
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-06-11 21:24:01 +0530

    npm package installed and imported

--- notes app/app.js
+++ notes app/app.js
@@ -23,8 +23,9 @@
 }
 
 
-
+const validator = require('validator')
 const getNotes = require('./notes')
 
 const message = getNotes()
 console.log(message)
+console.log(validator.isEmail('a@gmail.com'))
