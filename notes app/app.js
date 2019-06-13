fb624fd285c9cd4125998485497ba91bbc1b0691
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-06-13 12:54:59 +0530

    using yargs to set commands in app

--- notes app/app.js
+++ notes app/app.js
@@ -1,34 +1,36 @@
-{//Notes
-    // // //loading another js file in app.js
-// // const myName = require('./utils')
-
-// // console.log(myName)
-
-// //calling a function from utils.js
-
-// const myName = require('./utils')
-
-// console.log(myName('Noob','Master'))
-
-/****************************/
-}
-
-{// challenge: Define and use a function in new file
-
-// 1. Create a new file called notes.js
-// 2. Create getNots function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From applicationCache.js load an and callthe 
-//    function printing message to the console
-}
-
-
-// const validator = require('validator')
-// const getNotes = require('./notes')
-
-// const message = getNotes()
-// console.log(message)
-// console.log(validator.isEmail('a@gmail.com'))
-
 const chalk = require('chalk')
-console.log(chalk.black.bgYellow.bold('Success!'))
+const yargs = require('yargs')
+const getNotes = require('./notes') 
+
+// if (command === 'add') {
+//     console.log('adding a note')
+// } else if(command === 'remove') {
+//     console.log('removing notes')
+// } else if(command === 'read') {
+//     console.log('reading notes')
+// }
+
+// add read remove list
+
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note')
+    }
+
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a  note',
+    handler: function () {
+        console.log('Removing a note')
+    }
+})
+
+
+console.log(yargs.argv)
