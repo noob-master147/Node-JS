fe8d907216964673ddd09562d7590e26500252e1
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-06-13 15:55:41 +0530

    readFileSynv in fs

--- playground/1-JSON.js
+++ playground/1-JSON.js
@@ -1,11 +1,16 @@
-const book = {
-    title: 'Ego is Enemy',
-    author: 'Ryan Holiday',
+const fs = require('fs')
+// const book = {
+//     title: 'Ego is Enemy',
+//     author: 'Ryan Holiday',
 
-}
+// }
 
-const bookJSON = JSON.stringify(book)
-console.log(bookJSON)
+// const bookJSON = JSON.stringify(book)
 
-const parsedData = JSON.parse(bookJSON)
-console.log(parsedData.author)
+// fs.writeFileSync('JSON-1.json', bookJSON)
+
+
+const dataBuffer = fs.readFileSync('JSON-1.json').toString()
+
+const data = JSON.parse(dataBuffer)
+console.log(data.title)
