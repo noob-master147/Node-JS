33101b801ddb73f15af4ea5ca677e6e380ef1e98
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-06-14 17:40:29 +0530

    async demonstration

---
+++ weather-app/app.js
@@ -0,0 +1,10 @@
+// Generally the program is in synchronus mode
+// but it can be made async
+
+console.log('starting')
+
+setTimeout(()=>{
+    console.log('2 sec')
+}, 2000)
+
+console.log('stopping')
