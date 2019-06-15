5f3f58008b971c5939551364fbb34839e6dd8cff
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-06-15 23:29:14 +0530

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,40 +1,21 @@
 const request = require('request')
+const geoCode = require('./utils/geocode')
+const getForcast = require('./utils/forcast')
 
-var  url = 'https://api.darksky.net/forecast/8cc1e6c21f1bb24f693856b227882375/28.7041,77.1025?units=si'
-request({ url: url, json: true }, (error, response) => {
 
+
+geoCode('Lucknow', (error, location) => {
     if (error) {
-        console.log('Unable to connect to weather service')
-    } else if(response.body.error) {
-        console.log(response.body.error)
-        console.log('Unable to connect to weather service')
+        console.log('error:', error)
     } else {
-        var temp = response.body.currently.temperature
-        var precipProb = response.body.currently.precipProbability
-        console.log(temp)
-        console.log(precipProb)
+        console.log('data:', location)
+        getForcast(location.longitude, location.latitude, (error, data) => {
+            if (error) {
+                console.log('error:', error)
+            } else {
+                console.log('data:', data)
+            }
+    })
     }
-}) 
-
-// Geo coding 
-
-var Gcode = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Lucknow.json?limit=1&access_token=pk.eyJ1Ijoibm9vYi1tYXN0ZXIxNDciLCJhIjoiY2p3eGNsNnV5MHQ0ajQ4bnQ4OWJwOXA5OCJ9.nev61Q8X_Phd4REhg-0KLg'
-request({ url: Gcode, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location service')
-    } else if(response.body.features.length == 0) {
-        console.log('location not found.\nTry some new keywords')
-    } else {
-        var longitude = response.body.features[0].center[0]
-        var latitude = response.body.features[0].center[1]
-        console.log(longitude)
-        console.log(latitude)
-    }
+    
 })
-
-
-
-
-
-
-
