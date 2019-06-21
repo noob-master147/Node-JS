6fa9724d51a0d881ec51caf119a1308f3f305305
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-06-21 20:32:13 +0530

    templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,44 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
-app.get('',(req, res) => {
-    res.send('Hello express')
-})
 
-app.get('/help' , (req, res) => {
-    res.send('Help Page')
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index' ,{
+        title: 'Weather App',
+        name: 'Noob Master'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('about the page')
+    res.render('about', {
+        title: 'About Me',
+        name: 'Noob Master'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Available',
+        name: 'Noob Master'
+    })
 })
 
 app.get('/weather', (req, res) => {
-    releaseEvents.send('weather page')
+    res.send({
+        forcast: 'it is snowing',
+        location: 'Noida'
+    })
 })
 
-//app.com
-//app.com/help
-//app.com/about
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
