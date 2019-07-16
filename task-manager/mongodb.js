20cd4c9c06f0749228ccd99dcd339d85a43f5ec6
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-07-16 17:58:10 +0530

    promises used in update

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,6 @@
-//CRUD operation
+//CRUD operations on mongodb
 
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID =mongodb.ObjectID
-
-const {MongoClient, ObjectID} = require('mongodb')
+const { MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -15,13 +11,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
     const db = client.db(databaseName)
 
-    db.collection('users').findOne({name: 'devang'}, (error, user) => {
-        if(error) {
-            return console.log('Unable to fetch')
+    db.collection('users').updateOne({
+        _id: new ObjectID("5d2866130a8a942933aedbe2")
+    }, {
+        $set: {
+            name: 'Andrew'
         }
-
-        console.log(user)
+    }).then((result) => {
+        console.log(result.modifiedCount)
+    }).catch((error) => {
+        console.log(error)
     })
-
 
 })
