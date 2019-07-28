792e95fc8ffd4f5920fc9c59e1be0c587a2adc6e
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-07-28 23:48:12 +0530

    promise chaining

---
+++ task-manager/playground/promise-chaining.js
@@ -0,0 +1,17 @@
+require('../src/db/mongoose')
+ const User = require('../src/models/user')
+
+//  5d2ebda0e0d93b30fbf322b8
+
+User.findByIdAndUpdate('5d2ebda0e0d93b30fbf322b8',{
+    'age': 20,
+}).then((user) => {
+    console.log(user)
+    return User.countDocuments({
+        'age':20
+    }).then((result) => {
+        console.log(result)
+    })
+}).catch((e) => {
+    console.log(e)
+})
