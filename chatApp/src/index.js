f3a4212f05f8f7a939b1918d62a36ab9f805b542
Divyansh <divyanshkhandelwal147@gmail.com>
2020-03-19 11:07:15 +0530

    socket.io is setup and working

--- chatApp/src/index.js
+++ chatApp/src/index.js
@@ -1,2 +1,23 @@
-const route = require('express')()
+const express = require('express')
 const chalk = require('chalk')
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', () => {
+    console.log(chalk.bgGreen.black('New WebSocket Connection'))
+})
+
+server.listen(port, ()=> {
+    console.log(chalk.bgYellow.black(` Server is up and running on Port ${port} `))
+})
